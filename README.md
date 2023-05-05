58a7440fa70a1aa8dc28635ed1a677bf29ae0950
isgcl <isaguclu81@gmail.com>
2023-05-05 11:30:26 +0300

    some updates

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
 # wp-headless app
 
+<p><a href="https://headless.sipa.web.tr" target="_blank" rel="nofollow"><img style="width:60px;height:auto" src="https://headless.sipa.web.tr/favicon.png" alt="SIPA Agency"></a></p>
+
 <h2>React & WordPress Headless CMS App</h2>
 
-This is a React App for WordPress custom theme. 
+<p>This is a React App for WordPress custom theme. </p>
 
-<h3>V1 Notices</h3>
+<p><strong>See production site:</strong> <a href="https://headless.sipa.web.tr" target="_blank" rel="nofollow">headless.sipa.web.tr</a></p>
+
+<h3>V 1.0 Notices</h3>
 
 <ul>
-    <li> Post list, infinite scroll, post detail, post popup view </li>
+    <li> Post list, infinite scroll, post detail and comment, post popup view </li>
+    <li> Dark & Ligt Mode Options</li>
+    <li> User Registiration & Login Implementation </li>
     <li> Page structure </li>
     <li> SEO & SMO Optimization</li>
 </ul>
--- src/components/pages/categories.js
+++ src/components/pages/categories.js
@@ -8,7 +8,7 @@ import parseHtml from 'html-react-parser';
 import PostsForGrid from './post-for-grid';
 
 
-const CategoriesPageContent = ({display,postcount})=> {
+const CategoriesPageContent = ({display, postcount})=> {
   
 
   const [loading,setLoading] = useState(true)
@@ -22,14 +22,13 @@ const CategoriesPageContent = ({display,postcount})=> {
         Authorization : null
     }
   }
-
   
   const getCategories = async ()=>{
     await axios.get(process.env.REACT_APP_WP_HEADLESS_URI+'categories/?hide_empty=true&page='+currentPageNo,noAuth)
     .then(res => {
         if (res.data.length > 0) {
-            setCategories((prevdata)=>[...prevdata,...res.data])
-            setCurrentPageNo((prev)=>prev+1)
+            setCategories([...categories, ...res.data]);
+            setCurrentPageNo(currentPageNo + 1);
         } else {
             setHasMore(false)
         }
@@ -45,48 +44,49 @@ const CategoriesPageContent = ({display,postcount})=> {
     }) 
   }
 
-
   useEffect(()=>{
     getCategories()
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[])
+  },[false]);
 
   return (
     <>
     {
-        loading ? <Processing message={'Loading...'} /> :
-        <section id='categories'>
-        <InfiniteScroll
-          dataLength={categories.length}
-          next={getCategories}
-          hasMore={hasMore}
-          loader={<Processing message={'Loading categories...'} />}
-          className= { display === 'postin' ? 'category-selection' : 'grid-article grid-categories'}
-          endMessage={ <></> }
-        >
-         { (display === undefined || display === 'title') &&
-           categories.map((cat,ind)=> cat.count > 0 && <article key={ind}><h2>{parseHtml(cat.name)}</h2> <Link to={`/cat/${cat.slug}`} className='stick'>{parseHtml(cat.name)}</Link></article> )
-         }
-         { display === 'excerpt' &&
-           categories.map((cat,ind)=> cat.count > 0 && <article key={ind}><h2>{parseHtml(cat.name)}</h2> {cat.description ? `${parseHtml(cat.description)}` : null } <Link to={`/cat/${cat.slug}`} className='stick'>{parseHtml(cat.name)}</Link></article> )
-         }
-         { display === 'thumbnail' &&
-           categories.map((cat,ind)=> cat.count > 0 && <article key={ind} className={ind % 3 === 2 && 'pick'}><h2>{parseHtml(cat.name)}</h2> <picture><img src={cat.acf.cat_img_url} alt={parseHtml(cat.name)} /></picture> {cat.description ? <p>{parseHtml(cat.description)}</p> : null } <Link to={`/cat/${cat.slug}`} className='stick'>{parseHtml(cat.name)}</Link></article> )
-         }
-         { display === 'postin' &&
-           categories.map((cat,ind)=> cat.count > 0 &&
-           <>
-                <h2><span><em>{parseHtml(cat.name)}</em></span> <Link to={`/cat/${cat.slug}`} className='cat-link'> See all {parseHtml(cat.name)} <i className='heady icon-right-big'></i> </Link> </h2>
-                <PostsForGrid key={ind} ind={ind} postcount={postcount} catslug={cat.slug} catname={parseHtml(cat.name)} catimg={cat.acf.cat_img_url} />
-           </>
-            )
-         }
-        </InfiniteScroll>
-        
-        {
-          errorLoading && <Processing message='An error occured. Connection failed. Please refresh' cssclass='error' />
-        }
-      </section> 
+        loading ? 
+          <Processing message={'Loading...'} /> 
+        :
+          <section id='categories'>
+            <InfiniteScroll
+              dataLength={categories.length}
+              next={getCategories}
+              hasMore={hasMore}
+              loader={<Processing message={'Loading categories...'} />}
+              className= { display === 'postin' ? 'category-selection' : 'grid-article grid-categories'}
+              endMessage={ <></> }
+            >
+            { (display === undefined || display === 'title') &&
+              categories.map((cat,ind)=> cat.count > 0 && <article key={ind}><h2>{parseHtml(cat.name)}</h2> <Link to={`/cat/${cat.slug}`} className='stick'>{parseHtml(cat.name)}</Link></article> )
+            }
+            { display === 'excerpt' &&
+              categories.map((cat,ind)=> cat.count > 0 && <article key={ind}><h2>{parseHtml(cat.name)}</h2> {cat.description ? `${parseHtml(cat.description)}` : null } <Link to={`/cat/${cat.slug}`} className='stick'>{parseHtml(cat.name)}</Link></article> )
+            }
+            { display === 'thumbnail' &&
+              categories.map((cat,ind)=> cat.count > 0 && <article key={ind} className={ind % 3 === 2 && 'pick'}><h2>{parseHtml(cat.name)}</h2> <picture><img src={cat.acf.cat_img_url} alt={parseHtml(cat.name)} /></picture> {cat.description ? <p>{parseHtml(cat.description)}</p> : null } <Link to={`/cat/${cat.slug}`} className='stick'>{parseHtml(cat.name)}</Link></article> )
+            }
+            { display === 'postin' &&
+              categories.map((cat,ind)=> cat.count > 0 &&
+              <>
+                    <h2><span><em>{parseHtml(cat.name)}</em></span> <Link to={`/cat/${cat.slug}`} className='cat-link'> See all {parseHtml(cat.name)} <i className='heady icon-right-big'></i> </Link> </h2>
+                    <PostsForGrid key={ind} ind={ind} postcount={postcount} catslug={cat.slug} catname={parseHtml(cat.name)} catimg={cat.acf.cat_img_url} />
+              </>
+                )
+            }
+            </InfiniteScroll>
+            
+            {
+              errorLoading && <Processing message='An error occured. Connection failed. Please refresh' cssclass='error' />
+            }
+        </section> 
     }
     
       </>
